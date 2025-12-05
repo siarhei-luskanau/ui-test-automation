6fbbfe7e6c222e365ed158b043fe66a0ed37f810
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2025-12-05 21:52:47 +0100

    Updated Multiplatform-App

--- Multiplatform-App/build.gradle.kts
+++ Multiplatform-App/build.gradle.kts
@@ -6,5 +6,4 @@ plugins {
     alias(libs.plugins.android.application).apply(false)
     alias(libs.plugins.android.kmp.library).apply(false)
     alias(libs.plugins.kotlin.jvm).apply(false)
-    alias(libs.plugins.compose.hot.reload).apply(false)
 }
--- Multiplatform-App/settings.gradle.kts
+++ Multiplatform-App/settings.gradle.kts
@@ -28,11 +28,6 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-plugins {
-    // https://github.com/JetBrains/compose-hot-reload?tab=readme-ov-file#set-up-automatic-provisioning-of-the-jetbrains-runtime-jbr-via-gradle
-    id("org.gradle.toolchains.foojay-resolver-convention").version("1.0.0")
-}
-
 include(":sharedUI")
 include(":androidApp")
 include(":desktopApp")
